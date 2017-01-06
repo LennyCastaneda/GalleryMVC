2a805a18314dbe905960ba2c405ab98b2113a1bb
Lenny Castaneda <lenny.castaneda@gmail.com>
2017-01-06 03:03:56 -0800

    Fine tunes layout and grid view...hard coded data to get it to populate and test and debug.

--- src/GalleryMVC/Controllers/GalleryController.cs
+++ src/GalleryMVC/Controllers/GalleryController.cs
@@ -10,7 +10,25 @@ namespace GalleryMVC.Controllers
     {
         public ActionResult Grid()
         {
+            ViewBag.MovieTitle = "The Amazing Spiderman (2012)";
+            ViewBag.Description = "After Peter Parker is bitten by a genetically altered spider, he gains newfound, spider-like powers and ventures out to solve the mystery of his parent's mysterious death.";
+            ViewBag.CastStars = new string[]
+            {
+                "Andrew Garfield: Sprider-Man / Peter Parker",
+                "Emma Stone: Gwen Stacy",
+                "Rhys Ifans: The Lizard / Dr. Curt Connors",
+                "Denis Leary: Captain Stacy",
+                "Martin Sheen: Uncle Ben",
+                "Sally Field: Aunt May",
+                "Irrfan Khan: Rajit Ratha",
+                "Campbell Scott: Rajita Ratha",
+                "Embeth Davidtz: Mary Parker",
+                "Chris Zylka: Flash Thompson",
+                "Max Charles: Peter Parker (Age 4)",
+                "C. Thomas Howell: Jack's Father"
+            };
+
             return View();
         }
     }
-}
+} 
